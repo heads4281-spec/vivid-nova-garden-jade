aeb6d8aed7d92c5b2d46aca541a5a06fbe6a3346
heads4281-spec <heads4281@gmail.com>
2026-08-22 12:28:27 +0100

    Deploy complete 3D animation engine with root layout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import { createFileRoute } from '@tanstack/react-router'
 import { useState, useRef } from 'react'
 import { AnimationEngine } from '../components/AnimationEngine'
 
-export const Route = createFileRoute('/')' ({
+export const Route = createFileRoute('/')({  
   component: AnimationPage,
 })
 
